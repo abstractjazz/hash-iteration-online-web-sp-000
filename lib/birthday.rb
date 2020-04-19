7d8aaf513672bb01f78d3c6b2fbbceb4e69466cf
Evan Burton <evan@heyevan.com>
2020-04-19 08:09:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@
       if age < 12
         puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
       else
-        puts "You are too old for this." 
+        puts "You are too old for this."
   end
-end 
+end
 end 
